c0f4ed07eeff8c05663a0e327dbea6fb7b3594ea
junior sibusiso ngondo <juniorthemba1@gmail.com>
2024-01-07 22:36:26 +0000

    quick update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,28 @@
 #!/usr/bin/python3
-
 """
-    Determines if a given data set represents a valid UTF-8 encoding.
-    Args:
-        data (list): List of integers representing the bytes of the .
-    Returns:
-        bool: True if the data is a valid UTF-8 encoding, False otherwise.
+UTF-8 Validation
 """
 
 
-def validUTF8(data):
-
+def validUTF8(data) -> bool:
     """
-    Check if data os a valid UTF-8 encoding
-
-    Args:
-        data: A list of bytes.
-
-    Returns:
-        True if data is a valid UTF-8 encoding, False otherwise.
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-
-    for b in data:
-        if b >> 7 == 0:
-            continue
-        elif b >> 5 == 0b110:
-            continue
-        elif b >> 4 == 0b1110:
-            continue
-        elif b >> 3 == 0b11110:
-            continue
+    num_bytes = 0
+    for byte in data:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
         else:
-            return False
-    return True
+            if byte >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
