f5de17ac4ab964d513b85e88ecef481202d40ce5
junior sibusiso ngondo <juniorthemba1@gmail.com>
2024-02-15 20:30:59 +0000

    out was maria

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,16 +5,20 @@ Define isWinner function, a solution to the Prime Game problem
 
 
 def primes(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
+    """
+    Return list of prime numbers between 1 and n inclusive
+    Args:
         n (int): upper boundary of range. lower boundary is always 1
     """
+    prime = []
     sieve = [True] * (n + 1)
-    sieve[0] = sieve[1] = False
-    for p in range(2, int(n**0.5) + 1):
+    # Sieve of Eratosthenes algorithm to find prime numbers
+    for p in range(2, n + 1):
         if sieve[p]:
-            sieve[p*p:n+1:p] = [False] * len(sieve[p*p:n+1:p])
-    return [i for i in range(n + 1) if sieve[i]]
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
@@ -24,23 +28,26 @@ def isWinner(x, nums):
         x (int): no. of rounds of game
         nums (int): upper limit of range for each round
     Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+        Name of winner (Maria or Ben) or None if the winner cannot be found
     """
     if x is None or nums is None or x == 0 or nums == []:
         return None
 
     Maria = Ben = 0
-    max_limit = max(nums)
-    all_primes = primes(max_limit)
-
+    # Loop through each round
     for i in range(x):
-        if all_primes.count(nums[i]) % 2 == 0:
+        prime = primes(nums[i])
+        # Count the number of primes in the current round
+        if len(prime) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
 
+    # Determine the winner based on the count of primes in all rounds
     if Maria > Ben:
         return 'Maria'
     elif Ben > Maria:
         return 'Ben'
+
+    # Return None if no winner is found
     return None
